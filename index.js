150f26a260c7f92c1f85dad122e5ba4d06a3814b
Jonas Berlin <jonas.berlin@nitorcreations.com>
2016-09-18 18:47:23 +0200

    Use stringify-object since console.x() does not recurse all the way

--- index.js
+++ index.js
@@ -4,11 +4,16 @@
 
 var ws = require("nodejs-websocket");
 var console = require("better-console");
+var stringifyObject = require("stringify-object");
+
+var conf = {inlineCharacterLimit: 1000000000};
 
 ws.createServer(function (conn) {
   conn.on("text", function (msg) {
     msg = JSON.parse(msg);
-    console[msg.level].apply(console, msg.args);
+    var str = stringifyObject(msg.args, conf);
+    str = str.substring(1, str.length - 1);
+    console[msg.level].call(console, str);
   });
   conn.on("close", function (code, reason) {
     console.log("---------------------------------");
